62879b72eb9055181a95fd0c030907a052e21ef8
Park <tanagrit.sornchaiyagorn@gmail.com>
2024-02-21 15:56:59 +0700

    feature max-characters: add feature and a little usecase

--- max-characters/index.mjs
+++ max-characters/index.mjs
@@ -1,9 +1,50 @@
-// Problem
-// Given a string,
-// find and return the character that appears most frequently in the string.
+let str1 = "pPPark";
+let str2 = "cheeseCake";
+let str3 = undefined;
+let str4 = null;
+let str5 = "aBcDEfghijKLmnopqRsTuvWxyZ";
 
-// Examples:
-// getMaxCharacters("abcccccccd") === "c"
-// getMaxCharacters("apple 1231111") === "1"
+export const getMaxCharacters = (str) => {
+  console.log(str);
+  if (!str) {
+    if (str === "") str = "Blank string";
+    return `The string is invalid, value is ${str}`;
+  }
+  //if want to count in SeNsItvE CaSe,
+  //use | let newStr = str | instead
+  let newStr = str.toLowerCase();
+  //let newStr = str
+  let result = "";
+  let frequencyArray = [];
+  let frequencyObject = {};
+  let max = 1;
 
-export const getMaxCharacters = (str) => {};
+  for (let i = 0; i < newStr.length; i++) {
+    if (!frequencyArray.includes(newStr[i])) {
+      frequencyArray.push(newStr[i]);
+      frequencyObject[newStr[i]] = 1;
+    } else {
+      frequencyObject[newStr[i]] += 1;
+    }
+  }
+
+  for (let i in frequencyObject) {
+    if (frequencyObject[i] >= max) {
+      max = frequencyObject[i];
+      result = `${i} : ${max}`;
+    }
+  }
+
+  if (max == 1) {
+    result = "Max frequency is 1 , No have  most The string";
+  }
+  return result;
+};
+
+//test result in ,Multiple possibility
+console.log(getMaxCharacters(str1));
+console.log(getMaxCharacters(str2));
+console.log(getMaxCharacters(""));
+console.log(getMaxCharacters(str3));
+console.log(getMaxCharacters(str4));
+console.log(getMaxCharacters(str5));
